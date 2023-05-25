b91c998e25bc4aa632f21ff15ccd91cca929ea14
Eduardo Mendoza <eduardojmendoza@gmail.com>
2023-05-25 08:59:58 -0500

    Código base ORM JWT

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// In src/index.js
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const v1AuthRouter = require("./v1/routes/authRoutes");
+const v1superSecureResourceRouter = require("./v1/routes/superSecureResourceRoutes");
+
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use("/api/v1/auth", v1AuthRouter);
+app.use("/api/v1", v1superSecureResourceRouter);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+
+  V1SwaggerDocs(app, PORT);
+});
